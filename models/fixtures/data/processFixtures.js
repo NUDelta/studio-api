4b2634907b4b03e558154a0c3049f3fe60231798
Kapil Garg <kapil1garg@gmail.com>
2022-02-16 04:10:44 -0600

    fixing timezone issue

--- models/fixtures/data/processFixtures.js
+++ models/fixtures/data/processFixtures.js
@@ -3,32 +3,32 @@ import { DateTime } from "luxon";
 export const sprintProcessData = [
   {
     name: "Sprint 0",
-    start_day: DateTime.fromISO("2022-01-03T00:00:00-05:00", { setZone: true }),
-    end_day: DateTime.fromISO("2022-01-09T23:59:59-05:00", { setZone: true })
+    start_day: DateTime.fromISO("2022-01-03T00:00:00", { zone: "America/Chicago" }),
+    end_day: DateTime.fromISO("2022-01-09T23:59:59", { zone: "America/Chicago" })
   },
   {
     name: "Sprint 1",
-    start_day: DateTime.fromISO("2022-01-10T00:00:00-05:00", { setZone: true }),
-    end_day: DateTime.fromISO("2022-01-23T23:59:59-05:00", { setZone: true })
+    start_day: DateTime.fromISO("2022-01-10T00:00:00", { zone: "America/Chicago" }),
+    end_day: DateTime.fromISO("2022-01-23T23:59:59", { zone: "America/Chicago" })
   },
   {
     name: "Sprint 2",
-    start_day: DateTime.fromISO("2022-01-24T00:00:00-05:00", { setZone: true }),
-    end_day: DateTime.fromISO("2022-02-06T23:59:59-05:00", { setZone: true })
+    start_day: DateTime.fromISO("2022-01-24T00:00:00", { zone: "America/Chicago" }),
+    end_day: DateTime.fromISO("2022-02-06T23:59:59", { zone: "America/Chicago" })
   },
   {
     name: "Sprint 3",
-    start_day: DateTime.fromISO("2022-02-07T00:00:00-05:00", { setZone: true }),
-    end_day: DateTime.fromISO("2022-02-20T23:59:59-05:00", { setZone: true })
+    start_day: DateTime.fromISO("2022-02-07T00:00:00", { zone: "America/Chicago" }),
+    end_day: DateTime.fromISO("2022-02-20T23:59:59", { zone: "America/Chicago" })
   },
   {
     name: "Sprint 4",
-    start_day: DateTime.fromISO("2022-02-21T00:00:00-05:00", { setZone: true }),
-    end_day: DateTime.fromISO("2022-03-06T23:59:59-05:00", { setZone: true })
+    start_day: DateTime.fromISO("2022-02-21T00:00:00", { zone: "America/Chicago" }),
+    end_day: DateTime.fromISO("2022-03-06T23:59:59", { zone: "America/Chicago" })
   },
   {
     name: "Sprint 5",
-    start_day: DateTime.fromISO("2022-03-07T00:00:00-05:00", { setZone: true }),
-    end_day: DateTime.fromISO("2022-03-13T23:59:59-05:00", { setZone: true })
+    start_day: DateTime.fromISO("2022-03-07T00:00:00", { zone: "America/Chicago" }),
+    end_day: DateTime.fromISO("2022-03-13T23:59:59", { zone: "America/Chicago" })
   }
 ];
--- models/fixtures/data/projectFixtures.js
+++ models/fixtures/data/projectFixtures.js
@@ -12,7 +12,7 @@ export const projectData = [
   //       "" :
   //       "https://docs.google.com/spreadsheets/d/1IiNI6fisTZKpgg44lUWSpV7KROf_MCvlID9JgZZ3CUY",
   //   slack_channel: "proj-stash",
-  //   status_update_date: DateTime.fromISO("2021-04-30T12:00:00-05:00", { setZone: true })
+  //   status_update_date: DateTime.fromISO("2021-04-30T12:00:00", { zone: "America/Chicago" })
   // },
   {
     name: "MindYoga",
@@ -24,7 +24,7 @@ export const projectData = [
       "https://docs.google.com/spreadsheets/d/1d5_nydelgTsIRFej1qwmhhNyAevRcB19I_5QAVGuCNE" :
       "https://docs.google.com/spreadsheets/d/1qyFSd0kcLsFw0_FsyDUk0go9ua5qz1y_ElKfDdWqoS0",
     slack_channel: "proj-mindyoga",
-    status_update_date: DateTime.fromISO("2022-01-28T13:00:00-05:00", { setZone: true })
+    status_update_date: DateTime.fromISO("2022-01-28T13:00:00", { zone: "America/Chicago" })
   },
 
   // SIG NOT
@@ -38,7 +38,7 @@ export const projectData = [
       "https://docs.google.com/spreadsheets/d/1YGckE9U7CYrWAG6KSm_sVIkavkVdouDoqBJBuMJsDMY" :
       "https://docs.google.com/spreadsheets/d/17Gj3ApXxZ7fv556kg5CIq8gr16wcz_kaNW1JJ_tbCTE",
     slack_channel: "proj-os",
-    status_update_date: DateTime.fromISO("2022-02-11T13:00:00-05:00", { setZone: true })
+    status_update_date: DateTime.fromISO("2022-02-11T13:00:00", { zone: "America/Chicago" })
   },
   {
     name: "Interactive SOAP Notes",
@@ -50,7 +50,7 @@ export const projectData = [
       "https://docs.google.com/spreadsheets/d/1EMc1phCXpWd1FueNngun2b36bqK9hlh-nPpbH8X_8eI" :
       "https://docs.google.com/spreadsheets/d/1RcJOJ2g_fexlZMhQi8U-oW7iJJhjhOeqCsAx5GyATLY",
     slack_channel: "proj-soap",
-    status_update_date: DateTime.fromISO("2022-01-14T13:00:00-05:00", { setZone: true })
+    status_update_date: DateTime.fromISO("2022-01-14T13:00:00", { zone: "America/Chicago" })
   },
 
   // SIG CE
@@ -64,7 +64,7 @@ export const projectData = [
       "https://docs.google.com/spreadsheets/d/1zUTPStn9InIBtQ_TGaw84hH_VZxlDKII4_I8Z83niRE" :
       "https://docs.google.com/spreadsheets/d/1EuPW13DP2ICBDn9U9MC5m8Xv0FX1Y0FUi85KSkJdcvQ",
     slack_channel: "proj-cn",
-    status_update_date: DateTime.fromISO("2022-03-04T13:00:00-05:00", { setZone: true })
+    status_update_date: DateTime.fromISO("2022-03-04T13:00:00", { zone: "America/Chicago" })
   },
   {
     name: "CE for Relationship Development",
@@ -76,7 +76,7 @@ export const projectData = [
       "https://docs.google.com/spreadsheets/d/1Cf7p_vjox96LATpGERsGcoqhcH20I4HiTHWTtvAc1EQ" :
       "https://docs.google.com/spreadsheets/d/1LUfLfWzEpiFuaTyYvxzbKq7U6KeyP5i-M9KIPZd1rMA",
     slack_channel: "proj-ce-api",
-    status_update_date: DateTime.fromISO("2022-01-21T13:00:00-05:00", { setZone: true })
+    status_update_date: DateTime.fromISO("2022-01-21T13:00:00", { zone: "America/Chicago" })
   },
 
   // SIG RALE
@@ -90,7 +90,7 @@ export const projectData = [
       "https://docs.google.com/spreadsheets/d/1Uitdwd4XYcSiDN9H8x3Aldj_67IwwK_X68Pd_HpOtNg" :
       "https://docs.google.com/spreadsheets/d/1yA_GSz9WaYRr7np18raRWquLbM2oCJcDi3rjnkPovhw",
     slack_channel: "proj-knowledge-maps",
-    status_update_date: DateTime.fromISO("2022-02-25T13:00:00-05:00", { setZone: true })
+    status_update_date: DateTime.fromISO("2022-02-25T13:00:00", { zone: "America/Chicago" })
   },
   {
     name: "Scaffolded Exercises",
@@ -102,7 +102,7 @@ export const projectData = [
       "https://docs.google.com/spreadsheets/d/1O7NOeIAUMd8fyTS0ZrK4QAccu0nXY8lRi_IodoIolZs" :
       "https://docs.google.com/spreadsheets/d/1hFM-88Mk1qdmIq2E0lo76HGz_BEuns5Ug9rmWCfw4HQ",
     slack_channel: "proj-scaffolded-exercises",
-    status_update_date: DateTime.fromISO("2022-02-04T13:00:00-05:00", { setZone: true })
+    status_update_date: DateTime.fromISO("2022-02-04T13:00:00", { zone: "America/Chicago" })
   },
 
   // SIG CAMP
@@ -116,7 +116,7 @@ export const projectData = [
       "https://docs.google.com/spreadsheets/d/1JMlYA1z8J_BECWs8l7oqsP2hhQ5sE9XdJd9USprjUSY" :
       "https://docs.google.com/spreadsheets/d/1ye_2ImTEfuw6ydJxn5AZqpcIdp_EHuFbAY8Z7OiJWVI",
     slack_channel: "proj-qa-buddy",
-    status_update_date: DateTime.fromISO("2022-02-04T13:00:00-05:00", { setZone: true })
+    status_update_date: DateTime.fromISO("2022-02-04T13:00:00", { zone: "America/Chicago" })
   },
 
   // SIG BBQ
