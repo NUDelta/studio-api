9a8d2a71cbad93f1b98e249128d34b5305944f67
Kapil Garg <kapil1garg@gmail.com>
2022-03-07 04:43:28 -0600

    adding route to refresh data on server using fixtures

---
+++ routes/data.routes.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+import createPeopleFixtures from "../models/fixtures/populatePeople.js";
+import createProcessFixtures from "../models/fixtures/populateProcesses.js";
+import createProjectFixtures from "../models/fixtures/populateProjects.js";
+import createVenueFixtures from "../models/fixtures/populateVenues.js";
+import { prepopulateSprintCache } from "../controllers/sprints/sprintManager.js";
+
+export const dataRouter = new Router();
+
+/**
+ * Refreshes all data in MongoDB when called.
+ */
+dataRouter.get("/refreshData", async (req, res) => {
+  // clear existing data and refresh with fixtures
+  await createPeopleFixtures();
+  await createProcessFixtures();
+  await createProjectFixtures();
+  await createVenueFixtures();
+
+  // populate sprint cache after data is refreshed
+  await prepopulateSprintCache();
+
+  res.send("Data refreshed.");
+});
