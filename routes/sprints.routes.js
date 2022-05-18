b2cb3b9cd34176b61ace961d8c014b46f8352f4d
Kapil Garg <kapil1garg@gmail.com>
2022-05-18 07:42:58 +0000

    updating routing functions for sprint process

--- routes/sprints.routes.js
+++ routes/sprints.routes.js
@@ -3,6 +3,11 @@ import bodyParser from "body-parser";
 import { DateTime } from "luxon";
 
 import { Sprint } from "../models/processes/sprints.js";
+import {
+  fetchAllSprints,
+  fetchCurrentSprint, fetchSprintByDate,
+  fetchSprintByName
+} from "../controllers/processes/sprints/fetch.js";
 
 export const sprintRouter = new Router();
 
@@ -11,8 +16,7 @@ export const sprintRouter = new Router();
 // get all sprints
 sprintRouter.get("/", async (req, res) => {
   try {
-    let allSprints = await Sprint.find();
-    res.json(allSprints);
+    res.json(await fetchAllSprints());
   } catch (error) {
     res.send(`Error when fetching all sprints: ${ error }`);
   }
@@ -27,53 +31,45 @@ sprintRouter.get("/sprintByName", async (req, res) => {
       throw new Error("sprintName parameter not defined");
     }
 
-    // attempt to get a sprint process with the provided sprintName
-    let relevantSprint = await Sprint.findOne({ name: sprintName });
-    if (relevantSprint === null) {
-      throw new Error(`sprint not found for sprint name: ${ sprintName }`);
-    }
-
     // return sprint if found
-    res.json(relevantSprint);
-
+    res.json(await fetchSprintByName(sprintName));
   } catch (error) {
     res.send(`Error when fetching sprint by name: ${ error }`);
   }
 });
 
-// get the current sprint by date. date to look up sprint for can be optionally provided
+/**
+ * Gets the current sprint, based on the current time.
+ */
 sprintRouter.get("/currentSprint", async (req, res) => {
+  try {
+    // return sprint if found
+    res.json(await fetchCurrentSprint());
+  } catch (error) {
+    res.json(`Error when fetching current sprint: ${ error }`);
+  }
+});
+
+
+/**
+ * Gets the current sprint, based on the current time.
+ */
+sprintRouter.get("/sprintByDate", async (req, res) => {
   try {
     // check to see if a date was provided
     let dateString = req.query.timestamp;
     let dateObj;
 
-    // TODO: fix this...i dont think it's working rn
     // try to parse a DateTime from the string; otherwise, get the current date/time
     try {
       dateObj = DateTime.fromISO(dateString);
     } catch (error) {
-      console.error(`Error in parsing date for /currentSprint: ${ error }`);
-      dateObj = DateTime.now();
-    }
-
-    // using dateObj, find the sprint that we are currently in
-    let relevantSprint = await Sprint.findOne({
-      start_day: {
-        $lte: dateObj
-      },
-      end_day: {
-        $gte: dateObj
-      }
-    });
-
-    if (relevantSprint === null) {
-      throw new Error(`sprint not found for date: ${ dateObj }`);
+      throw new Error("timestamp parameter not defined");
     }
 
     // return sprint if found
-    res.json(relevantSprint);
+    res.json(await fetchSprintByDate(dateObj));
   } catch (error) {
-    res.json(`Error when fetching current sprint: ${ error }`);
+    res.json(`Error when fetching sprint by date: ${ error }`);
   }
 });
