4dd23e0c85ab0eaeda56cc3e31eaa5d24083fe6c
Kapil Garg <kapil1garg@gmail.com>
2023-12-21 00:15:07 +0000

    allowing for clock to be set for simulation

--- routes/data.routes.js
+++ routes/data.routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { populateData } from '../controllers/databaseManagement/refreshData.js';
+import sinon from 'sinon';
 
 export const dataRouter = new Router();
+let clock;
 
 /**
  * Refreshes all data in MongoDB when called.
@@ -12,6 +14,28 @@ dataRouter.get('/refreshData', async (req, res) => {
     res.send('Data refreshed.');
   } catch (error) {
     let msg = `Error in /data/refreshData: ${error.stack}`;
+    console.error(msg);
+    res.send(msg);
+  }
+});
+
+dataRouter.post('/setClock', async (req, res) => {
+  try {
+    clock = sinon.useFakeTimers({ now: new Date(req.body.newDate) });
+    res.send(`Clock set to ${JSON.stringify(clock)}`);
+  } catch (error) {
+    let msg = `Error in /data/setClock: ${error.stack}`;
+    console.error(msg);
+    res.send(msg);
+  }
+});
+
+dataRouter.post('/resetClock', async (req, res) => {
+  try {
+    clock.restore();
+    res.send(`Clock set to ${JSON.stringify(clock)}`);
+  } catch (error) {
+    let msg = `Error in /data/resetClock: ${error.stack}`;
     console.error(msg);
     res.send(msg);
   }
